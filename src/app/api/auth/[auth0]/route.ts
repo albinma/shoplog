3f9a886e764b7ae3d9b2d4a19c6900b15e0d7d3c
Albin Ma <albinma@gmail.com>
2024-06-18 10:22:31 -0400

    feat: poc for saving user data after login

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,16 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { Session, handleAuth, handleCallback } from '@auth0/nextjs-auth0';
+import { NextRequest } from 'next/server';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  callback: handleCallback({
+    afterCallback: async (
+      req: NextRequest,
+      session: Session,
+      state?: { [key: string]: any },
+    ) => {
+      // TODO: save user into db here
+      console.log(session.user, state);
+      return session;
+    },
+  }),
+});
